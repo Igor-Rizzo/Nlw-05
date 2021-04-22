d2101cb3d250953374afd9a877cdd46d39843eaf
Igor-Rizzo <igor.rizzo99@gmail.com>
2021-04-22 14:34:55 -0300

    Aulas 1 e 2

---
+++ src/pages/index.tsx
@@ -0,0 +1,55 @@
+//SPA  --------------------------------------------> EXEMPLO
+/**
+ * useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+      .then(response => response.json())
+      .then(data => console.log(data))
+
+  },[])
+ */
+//SSR ---------------------------------------------> EXEMPLO 
+/*
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    }
+  }
+ 
+}
+ */
+//SSG  ---------------------------------------------> EXEMPLO 
+/*export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 *60 * 8
+}
+*/
+
+export default function Home(props) {
+  console.log(props.episodes)
+  
+
+  return (
+    
+    <h1>Index</h1>
+  )
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes')
+  const data = await response.json()
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 *60 * 8 //Gerar uma nova requisição na api a cada 8 horas... E o HTML é statico usando getStaticProps. Só funciona em produção
+  }
+ 
+}
